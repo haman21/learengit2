ea1ef50ee2694d3e66f727d2bb42dd2d88d582b8
haman21 <ahmethaman2001@gmail.com>
2023-08-22 14:55:42 +0300

    copy2 diğer yarı tamamlamak

--- write.py
+++ write.py
@@ -9,4 +9,8 @@ print(3*'\n')
 print(5*' '+18*"*")
 for i in range(1,10):
     print(5*" "+i*"*"+(18-i*2)*" "+i*"*")
-print(3*'\n')
+
+
+for i in range(0,9):
+    print(5*" "+(9-i)*"*"+(i*2)*" "+(9-i)*"*")
+print(5*' '+18*"*")    
